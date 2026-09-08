e16b9dd1b6e7990504c08b8d77ae898cd450bfe6
kenny <meowla2.kk@gmail.com>
2026-09-08 22:03:22 +0800

    Remove test files and cache

--- test_local.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ["MONGODB_URI"] = "mongodb+srv://kenny:Qwer1234@cluster0.6abxj.mongodb.net/cbc_rate?retryWrites=true&w=majority"
-
-# Now import and run the app
-import uvicorn
-from api.rates import app
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8765)
